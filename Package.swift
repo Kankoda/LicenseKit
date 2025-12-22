07cbfaa8197fcdeebceca859a6303667562b9b05
Daniel Saidi <daniel.saidi@gmail.com>
2025-12-22 16:22:36 +0100

    Update package

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.1
 
 import PackageDescription
 
 let package = Package(
     name: "LicenseKit",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v13),
-        .tvOS(.v15),
-        .watchOS(.v8)
+        .iOS(.v16),
+        .macOS(.v14),
+        .tvOS(.v16),
+        .watchOS(.v10),
+        .visionOS(.v1)
     ],
     products: [
         .library(
@@ -19,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LicenseKit",
-            url: "https://github.com/Kankoda/LicenseKit-Binaries/releases/download/2.0.1/LicenseKit.zip",
-            checksum: "a6130defa8376d8384feeaf88ac0c188dd4c131a2b5d5b3466949d055f3d96b2"
+            url: "https://github.com/Kankoda/LicenseKit-Binaries/releases/download/2.1.0/LicenseKit.zip",
+            checksum: "e84792ef3399e236a4a363d149a479a3d80e9a8d1c66cea21180c62097fb86bd"
         )
     ]
 )
