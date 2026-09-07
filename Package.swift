7a4b1097c920aae3053c307741497568e43a2f66
Daniel Saidi <daniel.saidi@gmail.com>
2026-09-07 11:12:49 +0200

    Update package to 2.2.3

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "LicenseKit",
             url: "https://github.com/Kankoda/LicenseKit-Binaries/releases/download/2.2.3/LicenseKit.zip",
-            checksum: "922cda468b1f5c0e6447792516ef1393235d81e9221182275155cd8501feeeff"
+            checksum: "a988526cad8ed19d3f07e886fa4547b6f7f131cdaf3b39b81c7c638d86370cb8"
         )
     ]
 )
